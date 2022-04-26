5ef46ed7507963785ff04c4e200bf29a1067d57a
Andrew Gaffney <andrew@agaffney.org>
2022-04-26 14:50:27 -0500

    Initial implementation

---
+++ Makefile
@@ -0,0 +1,23 @@
+BINARY=cardano-submit-api
+
+# Determine root directory
+ROOT_DIR=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+
+# Gather all .go files for use in dependencies below
+GO_FILES=$(shell find $(ROOT_DIR) -name '*.go')
+
+# Build our program binary
+# Depends on GO_FILES to determine when rebuild is needed
+$(BINARY): $(GO_FILES)
+	# Needed to fetch new dependencies and add them to go.mod
+	go mod tidy
+	go build -o $(BINARY) ./cmd/$(BINARY)
+
+.PHONY: build image
+
+# Alias for building program binary
+build: $(BINARY)
+
+# Build docker image
+image: build
+	docker build -t cloudstruct/$(BINARY) .
---
+++ internal/config/config.go
@@ -0,0 +1,116 @@
+package config
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	TESTNET_MAGIC = 1097911063
+	MAINNET_MAGIC = 764824073
+)
+
+type Config struct {
+	Logging LoggingConfig `yaml:"logging"`
+	Api     ApiConfig     `yaml:"api"`
+	Node    NodeConfig    `yaml:"node"`
+}
+
+type LoggingConfig struct {
+	Level string `yaml:"level" envconfig:"LOGGING_LEVEL"`
+}
+
+type ApiConfig struct {
+	ListenAddress string `yaml:"address" envconfig:"API_LISTEN_ADDRESS"`
+	ListenPort    uint   `yaml:"port" envconfig:"API_LISTEN_PORT"`
+}
+
+type NodeConfig struct {
+	Network      string `yaml:"network" envconfig:"NODE_NETWORK"`
+	NetworkMagic uint32 `yaml:"networkMagic" envconfig:"NODE_NETWORK_MAGIC"`
+	Address      string `yaml:"address" envconfig:"NODE_ADDRESS"`
+	Port         uint   `yaml:"port" envconfig:"NODE_PORT"`
+	SocketPath   string `yaml:"socketPath" envconfig:"NODE_SOCKET_PATH"`
+}
+
+// Singleton config instance with default values
+var globalConfig = &Config{
+	Logging: LoggingConfig{
+		Level: "info",
+	},
+	Api: ApiConfig{
+		ListenAddress: "",
+		ListenPort:    8090,
+	},
+	Node: NodeConfig{
+		Network: "mainnet",
+	},
+}
+
+func Load(configFile string) (*Config, error) {
+	// Load config file as YAML if provided
+	if configFile != "" {
+		buf, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			return nil, fmt.Errorf("error reading config file: %s", err)
+		}
+		err = yaml.Unmarshal(buf, globalConfig)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing config file: %s", err)
+		}
+	}
+	// Load config values from environment variables
+	// We use "dummy" as the app name here to (mostly) prevent picking up env
+	// vars that we hadn't explicitly specified in annotations above
+	err := envconfig.Process("dummy", globalConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error processing environment: %s", err)
+	}
+	if err := globalConfig.populateNetworkMagic(); err != nil {
+		return nil, err
+	}
+	if err := globalConfig.checkNode(); err != nil {
+		return nil, err
+	}
+	return globalConfig, nil
+}
+
+// Return global config instance
+func GetConfig() *Config {
+	return globalConfig
+}
+
+func (c *Config) populateNetworkMagic() error {
+	if c.Node.Network != "" {
+		switch c.Node.Network {
+		case "testnet":
+			c.Node.NetworkMagic = TESTNET_MAGIC
+		case "mainnet":
+			c.Node.NetworkMagic = MAINNET_MAGIC
+		default:
+			return fmt.Errorf("unknown network: %s", c.Node.Network)
+		}
+	}
+	return nil
+}
+
+func (c *Config) checkNode() error {
+	if c.Node.SocketPath != "" {
+		// Check that node socket path exists
+		if _, err := os.Stat(c.Node.SocketPath); err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("node socket path does not exist: %s", c.Node.SocketPath)
+			} else {
+				return fmt.Errorf("unknown error checking if node socket path exists: %s", err)
+			}
+		}
+	} else if c.Node.Address != "" && c.Node.Port > 0 {
+		// TODO: add some validation for node address/port
+	} else {
+		return fmt.Errorf("you must specify either the UNIX socket path or the address/port for your cardano-node")
+	}
+	return nil
+}
