ff5b1c9719ef1adcb025cd251e97735194ce32cd
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-28 18:58:04 -0500

    fix: use errors and string concatenation to avoid fmt (#314)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- submit/tx.go
+++ submit/tx.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 package submit
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"time"
@@ -37,7 +38,7 @@ type Config struct {
 func SubmitTx(cfg *Config, txRawBytes []byte) (string, error) {
 	// Fail fast if timeout is too large
 	if cfg.Timeout > math.MaxInt64 {
-		return "", fmt.Errorf("given timeout too large")
+		return "", errors.New("given timeout too large")
 	}
 	// Determine transaction type (era)
 	txType, err := ledger.DetermineTransactionType(txRawBytes)
